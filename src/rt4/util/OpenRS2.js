b51fa20b664985fd129a30ff087c1ae93151b9c4
Pazaz <pazaz@itspazaz.com>
2023-06-28 12:16:20 -0400

    chore: Reverse caches.json order (asc) so I don't have to &match=1 for rev 530

--- src/rt4/util/OpenRS2.js
+++ src/rt4/util/OpenRS2.js
@@ -108,7 +108,7 @@ if (process.env.DEV_MODE != 1 || !fs.existsSync('data/caches.json')) {
     await downloadFile(`${OPENRS2_DOMAIN}/caches.json`, 'data/caches.json');
 }
 
-let caches = JSON.parse(fs.readFileSync('data/caches.json', 'ascii'));
+let caches = JSON.parse(fs.readFileSync('data/caches.json', 'ascii')).reverse();
 
 export function findCache(rev = -1, openrs2 = -1, match = 0, lang = 'en') {
     return caches.filter(c => (c.id == openrs2 || (c.builds.length && c.builds.findIndex(b => b.major == rev) !== -1)) && c.language == lang)[match];
