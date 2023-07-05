c89fa70e34dd660b3d9034a5d17f3096c854860a
Pazaz <pazaz@itspazaz.com>
2023-07-05 16:25:43 -0400

    fix: Multiple dummyitem values (not boolean)

--- src/rsdata/web/routes/dump/index.js
+++ src/rsdata/web/routes/dump/index.js
@@ -1232,7 +1232,7 @@ export default function (f, opts, next) {
             } else if (code === 95) {
                 out += `2dzan=${data[0]}\n`;
             } else if (code === 96) {
-                out += `dummyitem=yes\n`;
+                out += `dummyitem=${data[0]}\n`;
             } else if (code === 97) {
                 out += `certlink=obj_${data[0]}\n`;
             } else if (code === 98) {
